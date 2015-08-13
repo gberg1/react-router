036b3c0afa2827531ce7f5dddcd3d94522e1038e
David Goldberg <drgold1988@gmail.com>
2015-08-13 16:18:55 -0700

    [feat]: defines component to render view for a route that is not found in routes

---
+++ app/components/NotFound.js
@@ -0,0 +1,14 @@
+'use strict';
+
+import React from 'react';
+
+const NotFound = React.createClass({
+  render: function() {
+    return (
+      <h1>Sorry, page Not Found!</h1>
+    );
+  }
+});
+
+export default NotFound;
+
