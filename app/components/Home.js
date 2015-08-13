3cb9dd87b76e19534e6cc5b6f03912d058cb6ae6
David Goldberg <drgold1988@gmail.com>
2015-08-13 16:19:40 -0700

    [feat]: creates homepage component which is the  default route

---
+++ app/components/Home.js
@@ -0,0 +1,14 @@
+'use strict';
+
+import React from 'react';
+
+const Home = React.createClass({
+  render: function() {
+    return (
+      <h1>Welcome to the homepage!</h1>
+    );
+  }
+});
+
+export default Home;
+
