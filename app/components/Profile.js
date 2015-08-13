c631353b8765c6cfd282977634d57924f802a9d1
David Goldberg <drgold1988@gmail.com>
2015-08-13 16:20:21 -0700

    [feat]: creates component view for Profile page

---
+++ app/components/Profile.js
@@ -0,0 +1,14 @@
+'use strict';
+
+import React from 'react';
+
+const Profile = React.createClass({
+  render: function() {
+    return (
+      <h1>This is the profile page!</h1>
+    );
+  }
+});
+
+export default Profile;
+
