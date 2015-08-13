94d9e37b0c71671a0b51003299c02bd25bddd887
David Goldberg <drgold1988@gmail.com>
2015-08-13 16:21:25 -0700

    [feat]: creates component view for AddPlace page

---
+++ app/components/AddPlace.js
@@ -0,0 +1,13 @@
+'use strict';
+
+import React from 'react';
+
+const AddPlace = React.createClass({
+  render: function() {
+    return (
+      <h1>Add a place here!</h1>
+    );
+  }
+});
+
+export default AddPlace;
