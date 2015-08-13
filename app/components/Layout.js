1939444ac44d9f4983486d014d0bafb772e2b507
David Goldberg <drgold1988@gmail.com>
2015-08-13 16:18:16 -0700

    [feat]: uses RouteHandler and Navigation to generate layout for routing

---
+++ app/components/Layout.js
@@ -0,0 +1,19 @@
+'use strict';
+
+import React from 'react';
+import Router from 'react-router';
+import Navigation from './Navigation';
+
+const Layout = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <Navigation />
+        <Router.RouteHandler />
+      </div>
+    );
+  }
+});
+
+export default Layout;
+
