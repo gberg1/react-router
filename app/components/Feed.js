745439670a3abc8451a8c8ec0785b6385998a844
David Goldberg <drgold1988@gmail.com>
2015-08-13 16:20:55 -0700

    [feat]: creates component view for Feed page

---
+++ app/components/Feed.js
@@ -0,0 +1,13 @@
+'use strict';
+
+import React from 'react';
+
+const Feed = React.createClass({
+  render: function() {
+    return (
+      <h1>This is the feed page!</h1>
+    );
+  }
+});
+
+export default Feed;
