f74462a3f3e511bfc324780f7cd469bcd1f69989
David Goldberg <drgold1988@gmail.com>
2015-08-13 16:15:31 -0700

    [feat]: creates router.js to render root component to DOM

---
+++ app/router.js
@@ -0,0 +1,9 @@
+'use strict';
+
+import React from 'react';
+import Router from 'react-router';
+import Routes from './components/Routes';
+
+Router.run(Routes, Router.HistoryLocation, (Root) => {
+  React.render(<Root />, app);
+});
