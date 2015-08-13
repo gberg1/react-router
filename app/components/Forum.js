fa01a550cedbc16b32805dc479c75eee717bff5b
David Goldberg <drgold1988@gmail.com>
2015-08-13 16:21:13 -0700

    [feat]: creates component view for Forum page

---
+++ app/components/Forum.js
@@ -0,0 +1,13 @@
+'use strict';
+
+import React from 'react';
+
+const Forum = React.createClass({
+  render: function() {
+    return (
+      <h1>This is the forum page!</h1>
+    );
+  }
+});
+
+export default Forum;
